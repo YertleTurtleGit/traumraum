65a1158890bb7a58328e5ae86a9b1420d3525a10
YertleTurtleGit <github@galzone.de>
2021-01-22 18:09:30 +0100

    dshjf

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 "use strict";
-const videoElement = document.getElementsByClassName("input_video")[0];
-const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const WIDTH = 1280;
+const HEIGHT = 720;
+const videoElementA = (document.getElementById("input_video_a"));
+const videoElementB = (document.getElementById("input_video_b"));
+const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 function onResults(results) {
     if (results.multiHandLandmarks) {
@@ -14,26 +17,70 @@ function onResults(results) {
                lineWidth: 2,
             });*/
             canvasCtx.fillRect(canvasElement.clientWidth -
-                landmarks[8].x * canvasElement.clientWidth, landmarks[8].y * canvasElement.clientHeight, 10, 10);
+                landmarks[8].x * canvasElement.clientWidth, landmarks[8].y * canvasElement.clientHeight, 5, 5);
         }
     }
 }
-const hands = new Hands({
-    locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    },
-});
-hands.setOptions({
-    maxNumHands: 1,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5,
-});
-hands.onResults(onResults);
-const camera = new Camera(videoElement, {
-    onFrame: async () => {
-        await hands.send({ image: videoElement });
-    },
-    width: 1280,
-    height: 720,
-});
-camera.start();
+class HandsCamera {
+    constructor(cameraNumber = 0, videoElement) {
+        this.cameraNumber = cameraNumber;
+        this.videoElement = videoElement;
+        this.initialize();
+    }
+    getCameraId() {
+        if (this.cameraId === undefined) {
+            var cameraDevices = [];
+            var cThis = this;
+            navigator.mediaDevices.enumerateDevices().then(function (devices) {
+                for (var i = 0; i < devices.length; i++) {
+                    var device = devices[i];
+                    if (device.kind === "videoinput") {
+                        cameraDevices.push(device);
+                    }
+                }
+                cThis.cameraId = cameraDevices[cThis.cameraNumber].deviceId;
+                return cThis.cameraId;
+            });
+        }
+        else {
+            return this.cameraId;
+        }
+    }
+    initialize() {
+        this.hands = new Hands({
+            locateFile: (file) => {
+                return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+            },
+        });
+        this.hands.setOptions({
+            maxNumHands: 1,
+            minDetectionConfidence: 0.5,
+            minTrackingConfidence: 0.5,
+        });
+        this.hands.onResults(onResults);
+        var cThis = this;
+        navigator.mediaDevices
+            .getUserMedia({
+            video: {
+                width: WIDTH,
+                height: HEIGHT,
+                deviceId: { exact: cThis.getCameraId() },
+            },
+            audio: false,
+        })
+            .then(function () {
+            cThis.onFrame();
+        })
+            .catch(function (error) {
+            console.error("Failed to acquire camera feed: " + error);
+            alert("Failed to acquire camera feed: " + error);
+            throw error;
+        });
+    }
+    async onFrame() {
+        await this.hands.send({ image: this.videoElement });
+        console.log("ON FRAME!");
+        window.requestAnimationFrame(this.onFrame.bind(this));
+    }
+}
+const cameraA = new HandsCamera(0, videoElementA);
--- script.ts
+++ script.ts
@@ -1,7 +1,15 @@
 "use strict";
 
-const videoElement = document.getElementsByClassName("input_video")[0];
-const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const WIDTH = 1280;
+const HEIGHT = 720;
+
+const videoElementA = <HTMLVideoElement>(
+   document.getElementById("input_video_a")
+);
+const videoElementB = <HTMLVideoElement>(
+   document.getElementById("input_video_b")
+);
+const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 
 function onResults(results) {
@@ -19,30 +27,93 @@ function onResults(results) {
             canvasElement.clientWidth -
                landmarks[8].x * canvasElement.clientWidth,
             landmarks[8].y * canvasElement.clientHeight,
-            10,
-            10
+            5,
+            5
          );
       }
    }
 }
 
-const hands = new Hands({
-   locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-   },
-});
-hands.setOptions({
-   maxNumHands: 1,
-   minDetectionConfidence: 0.5,
-   minTrackingConfidence: 0.5,
-});
-hands.onResults(onResults);
+class HandsCamera {
+   private videoElement: HTMLVideoElement;
+   private cameraNumber: number;
+   private cameraId: string;
 
-const camera = new Camera(videoElement, {
+   private hands;
+
+   constructor(cameraNumber: number = 0, videoElement: HTMLVideoElement) {
+      this.cameraNumber = cameraNumber;
+      this.videoElement = videoElement;
+      this.initialize();
+   }
+
+   getCameraId(): string {
+      if (this.cameraId === undefined) {
+         var cameraDevices: MediaDeviceInfo[] = [];
+         var cThis = this;
+         navigator.mediaDevices.enumerateDevices().then(function (devices) {
+            for (var i = 0; i < devices.length; i++) {
+               var device = devices[i];
+               if (device.kind === "videoinput") {
+                  cameraDevices.push(device);
+               }
+            }
+            cThis.cameraId = cameraDevices[cThis.cameraNumber].deviceId;
+            return cThis.cameraId;
+         });
+      } else {
+         return this.cameraId;
+      }
+   }
+
+   initialize(): void {
+      this.hands = new Hands({
+         locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+         },
+      });
+      this.hands.setOptions({
+         maxNumHands: 1,
+         minDetectionConfidence: 0.5,
+         minTrackingConfidence: 0.5,
+      });
+      this.hands.onResults(onResults);
+
+      var cThis = this;
+
+      navigator.mediaDevices
+         .getUserMedia({
+            video: {
+               width: WIDTH,
+               height: HEIGHT,
+               deviceId: { exact: cThis.getCameraId() },
+            },
+            audio: false,
+         })
+         .then(function () {
+            cThis.onFrame();
+         })
+         .catch(function (error) {
+            console.error("Failed to acquire camera feed: " + error);
+            alert("Failed to acquire camera feed: " + error);
+            throw error;
+         });
+   }
+
+   async onFrame() {
+      await this.hands.send({ image: this.videoElement });
+      console.log("ON FRAME!");
+      window.requestAnimationFrame(this.onFrame.bind(this));
+   }
+}
+
+const cameraA: HandsCamera = new HandsCamera(0, videoElementA);
+
+/*const cameraA = new Camera(videoElementA, {
    onFrame: async () => {
-      await hands.send({ image: videoElement });
+      await hands.send({ image: videoElementA });
    },
    width: 1280,
    height: 720,
 });
-camera.start();
+cameraA.start();*/
